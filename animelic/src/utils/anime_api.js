13d965750c030172ffa18cc5d6c74f40451afe7c
Wen-Hao Chu <leochu111@gmail.com>
2024-02-13 02:52:21 +1100

    deploy test

--- animelic/src/utils/anime_api.js
+++ animelic/src/utils/anime_api.js
@@ -1,19 +1,37 @@
 
+// export function findAll() {
+//     return fetch(`/api/animes`)
+// }
+
+
+// export function findAllBySeason(season, year) {
+//     return fetch(`/api/animes/${season}/${year}`)
+// }
+
+// export function findAllArticle() {
+//     return fetch(`/api/animes/seasonal`)
+// }
+
+// export function findByKeyword(keyword) {
+//     return fetch(`/api/animes/${keyword}`)
+// }
+
+
 export function findAll() {
-    return fetch(`/api/animes`)
+    return fetch(`https://animelic.onrender.com/api/animes`)
 }
 
 
 export function findAllBySeason(season, year) {
-    return fetch(`/api/animes/${season}/${year}`)
+    return fetch(`https://animelic.onrender.com/api/animes/${season}/${year}`)
 }
 
 export function findAllArticle() {
-    return fetch(`/api/animes/seasonal`)
+    return fetch(`https://animelic.onrender.com/api/animes/seasonal`)
 }
 
 export function findByKeyword(keyword) {
-    return fetch(`/api/animes/${keyword}`)
+    return fetch(`https://animelic.onrender.com/api/animes/${keyword}`)
 }
 
 
