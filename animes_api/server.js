1598125ccf3d4215276255465a3d0607694a3893
Wen-Hao Chu <leochu111@gmail.com>
2024-02-13 01:14:15 +1100

    update deploy configuration

--- animes_api/server.js
+++ animes_api/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 const animesRouter = require('./routes/animes_router')
+const session = require('express-session')
 
 
 app.use(express.static('client'))
@@ -10,6 +11,7 @@ app.use(express.json())
 
 
 app.use('/api/animes', animesRouter)
+app.use(session({secret: process.env.SESSION_SECRET || "mistyrose", resave: false, saveUninitialized: true}))
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
